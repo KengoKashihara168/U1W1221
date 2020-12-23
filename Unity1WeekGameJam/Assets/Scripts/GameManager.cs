2229949185cc284555331becf44f0ae09578ee65
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-12-23 22:19:18 +0900

    Unityプロジェクトの追加
    わけあってプロトタイピングまで完了しているプロジェクトを追加

---
+++ Unity1WeekGameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float TimeLimit = 0.0f;
+    [SerializeField] private ShojiSample shoji = null;
+    [SerializeField] private Text resultText = null;
+    [SerializeField] private Text timeText = null;
+
+    private float time = 0.0f;
+    private bool isStop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        Debug.Log("GameManager:Initialize");
+        shoji.Initialize();
+        resultText.text = "";
+        timeText.text = (TimeLimit - time).ToString();
+        isStop = true;
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isStop)
+        {
+            if (!shoji.isAllBreak)
+            {
+                if (time <= TimeLimit)
+                {
+                    time += Time.deltaTime;
+                    shoji.UpdateShoji();
+                    timeText.text = (TimeLimit - time).ToString();
+                }
+                else
+                {
+                    Debug.Log("GameManager:time = " + time);
+                    Debug.Log("GameManager:残り障子枚数 = " + shoji.GetRemaindShoji());
+                    resultText.text = "残り障子枚数 = " + shoji.GetRemaindShoji();
+                    isStop = true;
+                }
+            }
+            else
+            {
+                Debug.Log("GameManager:クリアタイム = " + time);
+                resultText.text = "クリアタイム = " + time;
+                isStop = true;
+            }
+        }
+    }
+
+    public void OnClickByStart()
+    {
+        Initialize();
+        isStop = false;
+    }
+}
---
+++ Unity1WeekGameJam/Assets/Scripts/ShojiSample.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShojiSample : MonoBehaviour
+{
+    [SerializeField] private ShojiButton[] buttons = null;
+
+    public bool isAllBreak { get; private set; }
+
+    public void Initialize()
+    {
+        Debug.Log("Shoji:Initialize");
+        for(var i = 0; i < buttons.Length;i++)
+        {
+            buttons[i].Initialize();
+        }
+
+        isAllBreak = false;
+    }
+
+    public void UpdateShoji()
+    {
+        bool isNotBreak = false;
+        for (var i = 0; i < buttons.Length; i++)
+        {
+            if (!buttons[i].isBreak)
+            {
+                isNotBreak = true;
+            }
+        }
+
+        if(!isNotBreak)
+        {
+            isAllBreak = true;
+            Debug.Log("Shoji:すべて破られた");
+        }
+    }
+
+    public int GetRemaindShoji()
+    {
+        int remaind = 0;
+        for (var i = 0; i < buttons.Length; i++)
+        {
+            if (!buttons[i].isBreak)
+            {
+                remaind++;
+            }
+            buttons[i].Enabled();
+        }
+
+        return remaind;
+    }
+}
