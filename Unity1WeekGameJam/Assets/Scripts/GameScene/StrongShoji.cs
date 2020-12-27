5cf6562e4037b450dba85c060c73f33f98af89b6
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-12-28 00:54:06 +0900

    ボタン押下時のSEを追加
    タイトルシーンとリザルトシーンのボタンを押したときにSEが再生するように実装

--- Unity1WeekGameJam/Assets/Scripts/GameScene/StrongShoji.cs
+++ Unity1WeekGameJam/Assets/Scripts/GameScene/StrongShoji.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class StrongShoji : Shoji
 {
+    [SerializeField] private AudioClip damageSE = null;
+
     public override void Initialize()
     {
         base.Initialize();
         breakCount = 3;
+    }
+
+    protected override void BreakShoji()
+    {
+        AudioClip se = damageSE;
+        breakCount--;
+        if (breakCount <= 0)
+        {
+            image.sprite = breakSprite;
+            SetShojiEnabled(false);
+            isBreak = true;
+            se = breakSE;
+        }
+        audioSource.PlayOneShot(se);
     }
 }
